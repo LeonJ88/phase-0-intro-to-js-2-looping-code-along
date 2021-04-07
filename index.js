9f0857f1f9bb81c2bd1e683f30d111808fce07cf
Javier <JavierRLeon@protonmail.com>
2021-04-07 15:19:15 -0400

    commit tree d8f220dafc28db358d223e843002a48e185b1659

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+
+function writeCards(names, eventName) {
+    let cards = []
+    for ( let i = 0; i < names.length; i++) {
+    cards.push(`Thank you, ${names[i]}, for the wonderful ${eventName} gift!`);
+    }
+    return cards;
+}
+
+
+function countDown(time) {
+    let i = time
+    while (i >= 0) {
+        console.log(i);
+        i--;
+    }
+    return time
+}
